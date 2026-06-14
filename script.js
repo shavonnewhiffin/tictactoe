faa42ec93f60bbdd0e9ee0e922622b502c9952a6
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-06-13 23:41:30 -0700

    added a few helpful notes

--- script.js
+++ script.js
@@ -6,47 +6,50 @@ let gameOver = false;
 let board = new Array(9)
 
 
-// Square Event listener
+//  Event listener for square clicks
 allSquares.forEach((square, i) => {
     square.addEventListener(("click"), () => {
+        // Block clicks if already full or game is over
         if (square.innerHTML || gameOver) {
             return;
         }
 
-        square.innerHTML = currentPlayer;
-        // Set element at the clicked index to the current player's symbol
+        // Set the selected square to current player's value, remember it, and display their symbol there
         board[i] = currentPlayer;
+        square.innerHTML = currentPlayer;
 
-        console.log(board);
-
-        if (checkWin()){
-            console.log("this runs")
-            title.innerHTML = `${currentPlayer} wins!`
+        // Every click, check for a win
+        if (checkWin()) {
+            title.innerHTML = `${currentPlayer} wins!`;
             return gameOver = true;
-            
         }
 
         if(checkDraw()){
-        title.innerHTML = `It's a tie!`
+        title.innerHTML = `It's a tie!`;
         return gameOver = true;
         }
-
-        currentPlayer = currentPlayer === "X" ? "O" : "X";
+        
+        switchPlayers();
         title.innerHTML = `${currentPlayer}'s turn`;
-    })
-}) 
+    });
+});
 
 function restartGame() {
+    // Reset the game
     gameOver = false;
+    // Reset the title font
     title.innerHTML = `${currentPlayer}'s turn`;
+    // Reset each square
     allSquares.forEach(square => {
         square.innerHTML = "";
     })
+    // Reset the board
     board = new Array(9)
 }
 
+// Loop through the board. If any squares in the board are empty its not a tie yet.
 function checkDraw() {
-    for (let i = 0; i <board.length; i++) {
+    for (let i = 0; i < board.length; i++) {
         if (board[i] === undefined) {
             return false;
         }
@@ -70,7 +73,8 @@ function checkWin() {
         [2, 4, 6]
     ]
 
-    for (let i = 0; i <winningIndicies.length; i++){
+    // Loop through winning combinations. Pull the 3 indicies from reach subarray and the symbol stored at those indicies. If 3 symbols are the same, its a win. If symbols are not the same, keep game going.
+    for (let i = 0; i < winningIndicies.length; i++){
         const matchingIndicies = winningIndicies[i];
         let symbol1 = board[matchingIndicies[0]]
         let symbol2 = board[matchingIndicies[1]]
@@ -85,5 +89,9 @@ function checkWin() {
             return true;
         }
     }
+}
+
+function switchPlayers() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
 }
 
